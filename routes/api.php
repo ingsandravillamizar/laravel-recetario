d8d68b160a3d70adf545ea81b2183dd5e1bfd95c
svillamizar <ingsandravillamizar@github.com>
2023-10-12 14:33:13 -0500

    consulta a tablas por medio de rutas

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\RecipeController;
+use App\Http\Controllers\Api\TagController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+ });*/
+
+Route::get('categories',            [CategoryController::class, 'index']);
+Route::get('categories/{category}', [CategoryController::class, 'show']);
+
+Route::get('recipes',               [RecipeController::class, 'index']);
+Route::get('recipes/{recipe}',      [RecipeController::class, 'show']);
+
+Route::get('tags',                  [TagController::class, 'index']);
+Route::get('tags/{tag}',            [TagController::class, 'show']);
