cb539201a06cb7faa7b0813b0435c13655589e90
svillamizar <ingsandravillamizar@github.com>
2023-10-12 16:41:28 -0500

    definiendo las demas rutas

--- routes/api.php
+++ routes/api.php
@@ -16,15 +16,15 @@ use App\Http\Controllers\Api\TagController;
 |
 */
 
-/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
- });*/
-
 Route::get('categories',            [CategoryController::class, 'index']);
 Route::get('categories/{category}', [CategoryController::class, 'show']);
+Route::apiResource('categories', CategoryController::class);
 
 Route::get('recipes',               [RecipeController::class, 'index']);
 Route::get('recipes/{recipe}',      [RecipeController::class, 'show']);
+Route::apiResource('recipes', RecipeController::class);
+
 
 Route::get('tags',                  [TagController::class, 'index']);
 Route::get('tags/{tag}',            [TagController::class, 'show']);
+Route::apiResource('tags', TagController::class);
